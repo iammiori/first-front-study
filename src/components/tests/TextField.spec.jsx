bbd7a70f81b14afceb88bba32fd2c94045aae405
iammiori <immioriii@gmail.com>
2024-11-05 00:28:20 +0900

    test:  textfield unit test 추가

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -1,9 +1,16 @@
 import { screen } from '@testing-library/react';
-import React from 'react';
 
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('sample test.', async () => {
-  expect(1).toBe(1);
+/// AAA (Arrange, Act, Assert)
+it('className prop으로 설정한 css class가 적용된다', async () => {
+  // renderApi 호출
+  await render(<TextField className={'my-class'} />);
+
+  // className 이라는 내부 props, state 값을 검증 (X)
+  // 렌더링되는 DOM 구조가 올바르게 변경되었는지 확인 (O) -> 최종적으로 사용자가 보는 결과는 DOM
+  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+    'my-class',
+  );
 });
